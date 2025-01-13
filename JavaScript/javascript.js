f758e5b34842c500c3ea39b392ede08e3cb6bcaa
a24moiseslp <a24moiseslp@iessanclemente.net>
2025-01-13 17:39:29 +0100

    changes

---
+++ JavaScript/javascript.js
@@ -0,0 +1,24 @@
+
+function addTime(){
+    let add= document.getElementById("add");
+
+    count.innerText=+1;
+}
+
+function lessTime(){
+    let less= document.getElementById("less");
+
+    count.innerText=-1;
+}
+
+function resetTime(){
+    let reset= document.getElementById("reset");
+
+    reset.style.color="black";
+    count.innerText=0;
+}
+
+function newTime(){
+    let count= document.getElementById("count")
+
+}
