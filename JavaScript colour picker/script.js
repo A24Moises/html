85c8a206863298bc431479420c2f9f235a6e657b
a24moiseslp <a24moiseslp@iessanclemente.net>
2025-01-15 14:34:29 +0100

    new documents

---
+++ JavaScript colour picker/script.js
@@ -0,0 +1,13 @@
+function changeBg(){
+}
+
+function changeColor(){
+    let colorButton= document.getElementById("button");
+    let colorFigure= document.getElementById("figure");
+
+    colorFigure.style.background=("#b729aa");
+    colorButton.style.background=("#b729aa");
+    alert("COLOR CHANGE!")
+    
+
+}
