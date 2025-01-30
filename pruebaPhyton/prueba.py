214cfe490994dd856eb93e766948e76dd1f2e3e1
a24moiseslp <a24moiseslp@iessanclemente.net>
2025-01-30 12:54:32 +0100

    changes

--- pruebaPhyton/prueba.py
+++ pruebaPhyton/prueba.py
@@ -1,7 +1,9 @@
-#for i in range(1, 50):
-#    if i % 5 == 0 and i % 7 == 0:
-#       print(f"{i} es el divisor entre 5 y 7")
-#        break
+"""
+for i in range(1, 50):
+    if i % 5 == 0 and i % 7 == 0:
+       print(f"{i} es el divisor entre 5 y 7")
+        break
+"""
 """
 suma = 0
 while True:
@@ -15,7 +17,8 @@ while True:
 for i in range(1, entrada + 1):
     suma += i
 print("La suma es:", suma)
-"""   
+"""  
+""" 
 def totalSeg(min: int, seg: int):
     totalSeg =(min * 60) + seg
     return totalSeg
@@ -71,4 +74,74 @@ print("--------------------------------------------------------")
 print("La cantidad total de segundos que pasan de la hora introducida son:", totalSeg(entradaMin, entradaSeg), "seg")
 print("--------------------------------------------------------")
 print()
+"""
+"""
+entrada = int(input("Escribe un número entero: "))
+print()
 
+lista = []
+while entrada > 1:
+    lista.append(entrada)
+    print("Número ingresado:", lista)
+    print("----------")
+#--------------------------Pares---------------------------------------    
+    lista.extend([item for item in range(1, entrada) if item % 2 == 0])
+    lista.sort()
+    print("Lista de números pares que hay de 1 a",entrada,":",lista)
+    print()
+#--------------------Media de los numeros Pares-------------------------   
+    suma=0
+    for num in lista:
+        suma+=num
+        media=int(suma / len(lista))
+    print("La media de todos los numeros es:", media)
+    print("----------")
+#--------------------------Impares--------------------------------------    
+    lista.clear()
+    lista.append(entrada)
+    lista.extend([item for item in range(1, entrada) if item % 2 == 1])
+    lista.sort()
+    print("Lista de números impares que hay de 1 a",entrada,":",lista)
+    print()
+#---------------------Suma de todos los numeros-------------------------
+    lista.clear()
+    lista.append(entrada)
+    lista.extend([item for item in range(1, entrada)])
+    lista.sort()
+    suma=0
+    for num in lista:
+        suma+=num
+    print("La suma de todos los numeros es:", suma)
+    print("----------")
+    break
+"""
+"""
+def opPotencia (base,exponente):
+    if exponente == 0:
+        return 1
+    return base * opPotencia(base, exponente -1)
+
+def main():
+    print()
+    print("Calcula la potencia de un número")
+    print("----------")
+    base = int(input("Escribe la base: "))
+    print()
+    exponente = int(input("Escribe el exponente: "))
+    potencia = opPotencia(base,exponente)
+    print("----------")
+    print(f"{base} elevado a {exponente} vale {potencia}")
+    print("----------")
+
+main()
+"""
+def buscar(lista: List, objetivo)
+
+def main():
+    entrada= input("Ingresa valores, se agregaran a una lista, pueden ser letras o numeros: ")
+    valores= entrada.split()
+    valores_ordenados=sorted(valores, key=str)
+
+    print(valores_ordenados)
+
+main()
